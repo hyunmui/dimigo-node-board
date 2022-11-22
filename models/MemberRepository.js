d64476b5c8559c1dc3be12982f661ecc91c90695
Hyeonmin Seon <hmsun.es@esgroup.net>
2022-11-22 18:37:20 +0900

    repository 소스 제거

--- models/MemberRepository.js
+++ models/MemberRepository.js
@@ -3,7 +3,7 @@ const dbPromise = require('./DbConnection');
 module.exports = {
     getMember: async (memberId) => {
         const connection = await dbPromise;
-        let [rows] = await connection.query('SELECT * FROM members WHERE memberId = ?', [memberId]);
+        let [rows] = await connection.query('', []);
         return rows.length > 0 ? rows[0] : null;
     },
 };
--- models/PostRepository.js
+++ models/PostRepository.js
@@ -3,38 +3,29 @@ const dbPromise = require('./DbConnection');
 const postRepository = {
     findPosts: async () => {
         const connection = await dbPromise;
-        let [rows] = await connection.query(
-            `select p.*, m.nickname as author
-            from posts p
-            join members m on p.authorId = m.memberId
-        `
-        );
+        let [rows] = await connection.query('');
         return rows;
     },
     getPost: async (id) => {
         const connection = await dbPromise;
-        let [rows] = await connection.query('select * from posts where id = ?', [id]);
+        let [rows] = await connection.query('');
         return rows.length > 0 ? rows[0] : null;
     },
     savePost: async (post) => {
         const connection = await dbPromise;
         let oldPost = post.id ? await postRepository.getPost(post.id) : null;
         if (oldPost) {
-            await connection.query('update posts set title = ?, content = ? where id = ?', [
-                post.title,
-                post.content,
-                post.id,
-            ]);
+            await connection.query('', []);
             return post.id;
         } else {
             delete post.id;
-            let [results] = await connection.query('insert into posts set ?', post);
+            let [results] = await connection.query('');
             return results.insertId;
         }
     },
     deletePost: async (id) => {
         const connection = await dbPromise;
-        await connection.query('delete from posts where id = ?', [id]);
+        await connection.query('');
     },
 };
 
